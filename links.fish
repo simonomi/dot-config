2d8a3d11d08692fb26eb79ae15c421405a40b1ab
alice pellerin <thesimonpellerin@gmail.com>
2025-05-21 19:36:36 -0400

    update links to replace target files

--- links.fish
+++ links.fish
@@ -1,23 +1,22 @@
 #!/opt/homebrew/bin/fish
 
-link config.fish ~/.config/fish/config.fish
+ln -fw config.fish ~/.config/fish/config.fish
 
 mkdir -p ~/.config/nvim/syntax
-link nvim/init.vim ~/.config/nvim/init.vim
-link nvim/nvim.lua ~/.config/nvim/nvim.lua
-link nvim/coc-settings.json ~/.config/nvim/coc-settings.json
-link nvim/syntax/dex.vim ~/.config/nvim/syntax/dex.vim
-link nvim/syntax/kbd.vim ~/.config/nvim/syntax/kbd.vim
-link nvim/syntax/my_hex.vim ~/.config/nvim/syntax/my_hex.vim
+ln -fw nvim/init.vim ~/.config/nvim/init.vim
+ln -fw nvim/nvim.lua ~/.config/nvim/nvim.lua
+ln -fw nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -fw nvim/syntax/dex.vim ~/.config/nvim/syntax/dex.vim
+ln -fw nvim/syntax/kbd.vim ~/.config/nvim/syntax/kbd.vim
+ln -fw nvim/syntax/my_hex.vim ~/.config/nvim/syntax/my_hex.vim
 
-# TODO: change this path
 mkdir -p ~/.config/nushell
-link config.nu ~/.config/nushell/config.nu
+ln -fw config.nu ~/.config/nushell/config.nu
 
 mkdir -p ~/.config/jj
-link jj\ config.toml ~/.config/jj/config.toml
+ln -fw jj\ config.toml ~/.config/jj/config.toml
 
 mkdir -p ~/.config/helix/themes
-link helix/helix\ config.toml ~/.config/helix/config.toml
-link helix/helix_theme.toml ~/.config/helix/themes/helix_theme.toml
+ln -fw helix/helix\ config.toml ~/.config/helix/config.toml
+ln -fw helix/helix_theme.toml ~/.config/helix/themes/helix_theme.toml
 
