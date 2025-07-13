96482babe6c70728cb3714cff665f116ef0bad79
alice pellerin <thesimonpellerin@gmail.com>
2025-07-13 02:59:55 -0400

    use symbolic links

--- links.fish
+++ links.fish
@@ -1,38 +1,38 @@
 #!/opt/homebrew/bin/fish
 
-ln -fw config.fish ~/.config/fish/config.fish
+ln -sfw "$(pwd)/config.fish" ~/.config/fish/config.fish
 
 mkdir -p ~/.config/nvim/syntax
-ln -fw nvim/init.vim ~/.config/nvim/init.vim
-ln -fw nvim/nvim.lua ~/.config/nvim/nvim.lua
-ln -fw nvim/coc-settings.json ~/.config/nvim/coc-settings.json
-ln -fw nvim/syntax/dex.vim ~/.config/nvim/syntax/dex.vim
-ln -fw nvim/syntax/kbd.vim ~/.config/nvim/syntax/kbd.vim
-ln -fw nvim/syntax/my_hex.vim ~/.config/nvim/syntax/my_hex.vim
+ln -sfw "$(pwd)/nvim/init.vim" ~/.config/nvim/init.vim
+ln -sfw "$(pwd)/nvim/nvim.lua" ~/.config/nvim/nvim.lua
+ln -sfw "$(pwd)/nvim/coc-settings.json" ~/.config/nvim/coc-settings.json
+ln -sfw "$(pwd)/nvim/syntax/dex.vim" ~/.config/nvim/syntax/dex.vim
+ln -sfw "$(pwd)/nvim/syntax/kbd.vim" ~/.config/nvim/syntax/kbd.vim
+ln -sfw "$(pwd)/nvim/syntax/my_hex.vim" ~/.config/nvim/syntax/my_hex.vim
 
 mkdir -p ~/.config/nushell
-ln -fw nushell/config.nu ~/.config/nushell/config.nu
-ln -fw nushell/starship.nu ~/.config/nushell/starship.nu
-ln -fw nushell/zoxide.nu ~/.config/nushell/zoxide.nu
+ln -sfw "$(pwd)/nushell/config.nu" ~/.config/nushell/config.nu
+ln -sfw "$(pwd)/nushell/starship.nu" ~/.config/nushell/starship.nu
+ln -sfw "$(pwd)/nushell/zoxide.nu" ~/.config/nushell/zoxide.nu
 
 mkdir -p ~/.config/jj
-ln -fw jj\ config.toml ~/.config/jj/config.toml
+ln -sfw "$(pwd)/jj config.toml" ~/.config/jj/config.toml
 
 mkdir -p ~/.config/helix/themes
-ln -fw helix/helix\ config.toml ~/.config/helix/config.toml
-ln -fw helix/helix_theme.toml ~/.config/helix/themes/helix_theme.toml
+ln -sfw "$(pwd)/helix/helix config.toml" ~/.config/helix/config.toml
+ln -sfw "$(pwd)/helix/helix_theme.toml" ~/.config/helix/themes/helix_theme.toml
 
-ln -fw starship.toml ~/.config/starship.toml
+ln -sfw "$(pwd)/starship.toml" ~/.config/starship.toml
 
-ln -fw bacon.toml ~/.config/bacon.toml
+ln -sfw "$(pwd)/bacon.toml" ~/.config/bacon.toml
 
 mkdir -p ~/.config/macchina
-ln -fw macchina.toml ~/.config/macchina/macchina.toml
+ln -sfw "$(pwd)/macchina.toml" ~/.config/macchina/macchina.toml
 
 # TODO: remove original espanso path in application support?
 mkdir -p ~/.config/espanso
 for file in espanso/**.*;
 	mkdir -p ~/.config/(dirname $file)
-	ln -fw $file ~/.config/$file
+	ln -sfw "$(pwd)/$file" ~/.config/$file
 end
 
