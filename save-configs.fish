519f7fc8e1c023bcf8a06c8e336acfd260b4587b
alice pellerin <thealicepellerin@gmail.com>
2026-04-20 15:45:53 -0500

    .

--- save-configs.fish
+++ save-configs.fish
@@ -1,20 +1,26 @@
 #!/opt/homebrew/bin/fish
 
-echo -s (set_color --bold) "TinyStart:" (set_color normal)
-diff tinystart.toml ~/.config/tinystart/settings.toml
-echo
+set --local tinyStartChanges "$(diff --color=always tinystart.toml ~/.config/tinystart/settings.toml)"
+set --local xcodeChanges "$(diff --color=always xcode.idekeybindings ~/Library/Developer/Xcode/UserData/KeyBindings/Default.idekeybindings)"
 
-echo -s (set_color --bold) "Xcode:" (set_color normal)
-diff xcode.idekeybindings ~/Library/Developer/Xcode/UserData/KeyBindings/Default.idekeybindings
-echo
-
-read --prompt-str "do you want to save these changes? (y/N): " --local saveChanges
-
-switch $saveChanges
-	case Y y
-		cp ~/.config/tinystart/settings.toml tinystart.toml
-		cp ~/Library/Developer/Xcode/UserData/KeyBindings/Default.idekeybindings xcode.idekeybindings
-	case "" N n
-		return 0
+if test -n "$tinyStartChanges"
+	echo -s (set_color --bold) "TinyStart:" (set_color normal)
+	echo "$tinyStartChanges"
 end
 
+if test -n "$xcodeChanges"
+	echo -s (set_color --bold) "Xcode:" (set_color normal)
+	echo "$xcodeChanges"
+end
+
+if test -n "$tinyStartChanges$xcodeChanges"
+	read --prompt-str "do you want to save these changes? (y/N): " --local saveChanges
+	
+	switch $saveChanges
+		case Y y
+			cp ~/.config/tinystart/settings.toml tinystart.toml
+			cp ~/Library/Developer/Xcode/UserData/KeyBindings/Default.idekeybindings xcode.idekeybindings
+		case "" N n
+			return 1
+	end
+end
